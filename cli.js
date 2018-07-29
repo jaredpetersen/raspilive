c199914d84a693298832bdfb392b993cffec172d
Jared Petersen <jaredtoddpetersen@gmail.com>
2018-07-29 07:25:44 +0000

    minor logging change and cli option fix

--- cli.js
+++ cli.js
@@ -21,7 +21,7 @@ program
   .command('start')
   .description('start streaming video from the raspberry pi camera module')
   .option('-d, --directory <directory>', 'streaming video file hosting location', `${os.homedir()}/camera`)
-  .option('-f, --format [format]', 'video streaming format [hls, mpeg-dash]', /^(hls|mpeg-dash)$/i, 'hls')
+  .option('-f, --format <format>', 'video streaming format [hls, mpeg-dash]', /^(hls|mpeg-dash)$/i, 'hls')
   .option('-p, --port <port>', 'port number the server runs on', int, 8080)
   .action(({ directory, format, port }) => {
     server(directory, format, port);
