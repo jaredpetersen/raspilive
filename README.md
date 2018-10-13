e40fcc5e762cea13023e82e545df5b6d1c265236
Jared Petersen <jaredtoddpetersen@gmail.com>
2018-10-12 17:56:03 -0700

    fixed a time option implementation bug and improved latency

--- README.md
+++ README.md
@@ -91,7 +91,7 @@ The number of streaming video files included in the playlist.
 Defaults to `10`.
 
 ###### -s, --storage-size
-The number of streaming video files stored after they cycle out of the playlist. This is useful in cases where you want to look at previously recorded footage. The streaming video files are 2 seconds long so to have a 24-hour cycle of recorded video, specify `43200` (make sure to have enough storage space).
+The number of streaming video files stored after they cycle out of the playlist. This is useful in cases where you want to look at previously recorded footage. The streaming video files are 2 seconds long by default so to have a 24-hour cycle of recorded video, specify `43200` (make sure to have enough storage space).
 
 Defaults to `10`.
 
@@ -136,3 +136,7 @@ npx raspi-live --help
 raspi-live is only concerned with streaming video from the camera module and does not offer a playback solution.
 
 Browser support between the different streaming formats varies so in most cases a JavaScript playback library will be necessary. For more information on this, check out [Mozilla's article on the subject](https://developer.mozilla.org/en-US/docs/Web/Apps/Fundamentals/Audio_and_video_delivery/Live_streaming_web_audio_and_video).
+
+
+## Performance
+HLS and DASH inherently have latency baked into the technology. To reduce this, set the `time` option to 1 or .5 seconds and increase the list and storage size via the `list-size` and `storage-size` options. Ideally, there should be 12 seconds of video in the list and 50 seconds of video in storage.
--- cli.js
+++ cli.js
@@ -7,13 +7,10 @@ const os = require('os');
 const info = require('./package.json');
 const server = require('./lib/server');
 
-// Coercion function for integers
-const int = (value) => parseInt(value, 10);
-
-// Coercion function for integer range
+// Coercion function for number range
 const range = (min, max, value, def) => {
   if (value < min || value > max) return def;
-  return parseInt(value, 10);
+  return Number(value);
 };
 
 process.title = 'raspi-live';
@@ -28,17 +25,17 @@ program
   .description('start streaming video from the raspberry pi camera module')
   .option('-d, --directory <directory>', 'streaming video file hosting location', `${os.homedir()}/camera`)
   .option('-f, --format <format>', 'video streaming format [hls, dash]', /^(hls|dash)$/i, 'hls')
-  .option('-w, --width <width>', 'video resolution width', int, 1280)
-  .option('-h, --height <height>', 'video resolution height', int, 720)
-  .option('-r, --framerate <fps>', 'video frames per second', int, 25)
+  .option('-w, --width <width>', 'video resolution width', Number, 1280)
+  .option('-h, --height <height>', 'video resolution height', Number, 720)
+  .option('-r, --framerate <fps>', 'video frames per second', Number, 25)
   .option('-x, --horizontal-flip', 'flip the camera horizontally')
   .option('-y, --vertical-flip', 'flip the camera vertically')
   .option('-c, --compression-level <compression-level>', 'compression level [0-9]', range.bind(null, 0, 9), 9)
-  .option('-t, --time <time>', 'duration of streaming files', int, 2)
-  .option('-l, --list-size <list-size>', 'number of streaming files in the playlist', int, 10)
-  .option('-s, --storage-size <storage-size>', 'number of streaming files for storage purposes', int, 10)
-  .option('-p, --port <port>', 'port number the server runs on', int, 8080)
-  .action(({ directory, format, width, height, framerate, horizontalFlip = false, verticalFlip = false, compressionLevel, listSize, storageSize, port }) => {
+  .option('-t, --time <time>', 'duration of streaming files', Number, 2)
+  .option('-l, --list-size <list-size>', 'number of streaming files in the playlist', Number, 10)
+  .option('-s, --storage-size <storage-size>', 'number of streaming files for storage purposes', Number, 10)
+  .option('-p, --port <port>', 'port number the server runs on', Number, 8080)
+  .action(({ directory, format, width, height, framerate, horizontalFlip = false, verticalFlip = false, compressionLevel, time, listSize, storageSize, port }) => {
     console.log('configuration:', directory, format, width, height, framerate, horizontalFlip, verticalFlip, compressionLevel, time, listSize, storageSize, port);
     server(directory, format, width, height, framerate, horizontalFlip, verticalFlip, compressionLevel, time, listSize, storageSize, port);
   });
