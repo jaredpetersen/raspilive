3d437a90cf4bb0ac7185f33d2216103bb6b4ee2b
John Cassidy <johnwcassidy@gmail.com>
2020-05-12 01:34:10 -0400

    :bug: Fix issue where parameters not being provided

--- cli.js
+++ cli.js
@@ -36,8 +36,8 @@ program
   .option('-s, --storage-size <storage-size>', 'number of streaming files for storage purposes', Number, 10)
   .option('-p, --port <port>', 'port number the server runs on', Number, 8080)
   .option('-S, --secure', 'run with credentials for HTTPS')
-  .option('-C, --certificate <file>', 'path to SSL certificate', '')
-  .option('-K, --key <file>', 'path to private key', '')
+  .option('-C, --certificatePath <file>', 'path to SSL certificate', '')
+  .option('-K, --keyPath <file>', 'path to private key', '')
   .action(({ directory, format, width, height, framerate, horizontalFlip = false, verticalFlip = false, compressionLevel, time, listSize, storageSize, port, secure = false, certificatePath, keyPath}) => {
     console.log('configuration:', directory, format, width, height, framerate, horizontalFlip, verticalFlip, compressionLevel, time, listSize, storageSize, port, secure, certificatePath, keyPath);
     server(directory, format, width, height, framerate, horizontalFlip, verticalFlip, compressionLevel, time, listSize, storageSize, port, secure, certificatePath, keyPath);
