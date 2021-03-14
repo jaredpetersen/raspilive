ea7a9b13db2c5676148ec2e63b241daaa68a3e24
Jared Petersen <jaredtoddpetersen@gmail.com>
2021-03-14 15:02:59 -0700

    added debug flag

--- cmd/raspilive/main.go
+++ cmd/raspilive/main.go
@@ -17,16 +17,29 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 	cobra.EnableCommandSorting = false
 
+	var debug bool
+
 	rootCmd := &cobra.Command{
 		Use:   "raspilive",
 		Short: "raspilive is a livestreaming tool for the Raspberry Pi Camera Module",
 		Long: "raspilive is a livestreaming tool for the Raspberry Pi Camera Module\n\n" +
 			"For more information visit https://github.com/jaredpetersen/raspilive",
 		Version: "1.0.0",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			setLogLevel(debug)
+		},
 	}
 
 	rootCmd.AddCommand(hls.Cmd)
 	rootCmd.AddCommand(dash.Cmd)
 
+	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "enable debug logging")
+
 	rootCmd.Execute()
+}
+
+func setLogLevel(debug bool) {
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
 }
