784576062d61fe5747835395695bc65687d1834b
Jared Petersen <jaredtoddpetersen@gmail.com>
2021-03-14 21:26:47 -0700

    more restructuring

--- cmd/raspilive/main.go
+++ cmd/raspilive/main.go
@@ -4,20 +4,28 @@ import (
 	"os"
 	"time"
 
-	"github.com/jaredpetersen/raspilive/cmd/raspilive/dash"
-	"github.com/jaredpetersen/raspilive/cmd/raspilive/hls"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
-// TODO research application logging
+const serverShutdownDeadline = 10 * time.Second
+
+// VideoCfg represents the video configuration options
+type VideoCfg struct {
+	Width          int
+	Height         int
+	Fps            int
+	HorizontalFlip bool
+	VerticalFlip   bool
+}
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 	cobra.EnableCommandSorting = false
 
 	var debug bool
+	var video VideoCfg
 
 	rootCmd := &cobra.Command{
 		Use:   "raspilive",
@@ -30,9 +38,14 @@ func main() {
 		},
 	}
 
-	rootCmd.AddCommand(hls.Cmd)
-	rootCmd.AddCommand(dash.Cmd)
+	rootCmd.AddCommand(newHlsCmd(video))
+	rootCmd.AddCommand(newDashCmd(video))
 
+	rootCmd.PersistentFlags().IntVar(&video.Width, "width", 1920, "video width")
+	rootCmd.PersistentFlags().IntVar(&video.Height, "height", 1080, "video height")
+	rootCmd.PersistentFlags().IntVar(&video.Fps, "fps", 30, "video framerate")
+	rootCmd.PersistentFlags().BoolVar(&video.HorizontalFlip, "horizontal-flip", false, "horizontally flip video")
+	rootCmd.PersistentFlags().BoolVar(&video.VerticalFlip, "vertical-flip", false, "vertically flip video")
 	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "enable debug logging")
 
 	rootCmd.Execute()
